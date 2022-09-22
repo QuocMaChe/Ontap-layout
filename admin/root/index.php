cf557a640d0b32e28400ae3086ad797844206b66
Quoc Ma Che <92172767+QuocMaChe@users.noreply.github.com>
2022-09-22 23:27:33 +0700

    Create index.php

---
+++ admin/root/index.php
@@ -0,0 +1,17 @@
+<?php 
+	require '../check_login_admin.php';
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+Đây là khu vực admin
+<?php 
+include '../menu.php';
+ ?>
+</body>
+</html>
